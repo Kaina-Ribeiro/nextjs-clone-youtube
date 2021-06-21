45a1c0d18ab3e892a5fcff46d1e637865ad4d04b
Gabriel Ribeiro <55985861+Kaina-Ribeiro@users.noreply.github.com>
2021-06-21 12:03:55 -0300

    Layout Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div>
+    <Layout title="YouTube">
       Clone outube com Next.js + Material-UI
       <Button variant="outlined" color="secondary">
         Primary
       </Button>
-    </div>
+    </Layout>
   );
 }
